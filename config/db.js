68144504d85683dc8d7203538ed3526cf0520a83
Eferurhobo Temple voke <starukido@gmail.com>
2022-06-24 16:51:39 +0100

    heroku

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const MONGODB = process.env.MONGODB
-const ONLINEMONGODB = process.env.ONLINEMONGODB
-const MONGODBE = process.env.MONGODBE
+const ONLINEMONGODB = process.env.ONLINEMONGODB || "mongodb+srv://MrComputer:1234567890@cluster0.ohii5.mongodb.net/?retryWrites=true&w=majority"
+
+const MONGODBE = process.env.MONGODBE 
 
 const connectDB = async ()=>{
     try {
